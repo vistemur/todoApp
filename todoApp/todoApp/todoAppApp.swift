fff386edbe649a41a8948d5b760320298d843ac5
Roman Pozdnyakov <romanpozdnyakov@Romans-MacBook-Pro.local>
2023-06-17 00:27:33 +0300

    project created

---
+++ todoApp/todoApp/todoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAppApp.swift
+//  todoApp
+//
+//  Created by Roman Pozdnyakov on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct todoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
